79e9e1499db537ac0f838b24b626d195d199f91f
Machakhleli <ushangikakhidze@gmail.com>
2025-05-12 16:34:19 +0400

    done guess my number game

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 let minNum = 1;
 let maxNum = 20;
 //function for creating random number
@@ -7,10 +6,13 @@ let gettedRandomNum = randomNum(minNum, maxNum);
 // console.log(gettedRandomNum);
 
 const secretNum = document.querySelector(".secret-number");
-
 let inputedNum = document.querySelector("input");
 const bodyBg = document.querySelector("body");
 const displayedMsg = document.querySelector(".guessing");
+let storedHighscore = document.querySelector(".highscore");
+let startingScore = 20;
+let startingHighscore = localStorage.getItem("highscore");
+storedHighscore.textContent = `🥇 Highscore: ${startingHighscore}`;
 
 let checkInputedNum = document.querySelector(".check-btn");
 checkInputedNum.addEventListener("click", () => {
@@ -23,18 +25,42 @@ checkInputedNum.addEventListener("click", () => {
         bodyBg.style.backgroundSize = "cover";
         displayedMsg.textContent = "IT'S CORRECT NUMBER";
         displayedMsg.style.color = "white";
-        // document.querySelector("starting-score").style.color = "white";
-        // document.querySelector("highscore").style.color = "white";
+        checkInputedNum.style.color = "white";
+        checkInputedNum.disabled = true;
+        checkInputedNum.textContent = "you can't click now";
+        checkInputedNum.style.fontSize = "0.5rem";
+        inputedNum.style.color = "white";
+        inputedNum.disabled = true;
+        document.querySelector("span").style.color = "white";
+        document.querySelector("h1").style.color = "white";
+        document.querySelector(".starting-score").style.color = "white";
+        storedHighscore.style.color = "white";
+        restart.style.color = "white";
+
+        if (startingScore > startingHighscore) {
+            startingHighscore = startingScore;
+            localStorage.setItem("highscore", startingHighscore);
+            storedHighscore.textContent = `🥇 Highscore: ${startingHighscore}`;
+        }
+        if (startingScore == 0) {
+            document.querySelector(".starting-score").textContent = "You lose! Try again :)"
+        }
     } else if (enteredNum < minNum || enteredNum > maxNum) {
         displayedMsg.textContent = `You must enter numbers between ${minNum} and ${maxNum}.`;
-    } else if (enteredNum > gettedRandomNum) {
-        displayedMsg.textContent = "📈 Too high!";
-    } else if (enteredNum < gettedRandomNum) {
-        displayedMsg.textContent = "📉 Too low!";
+    } else {
+        if (startingScore > 1) {
+            displayedMsg.textContent = enteredNum < gettedRandomNum ? "📉 Too low!" : "📈 Too high!";
+            startingScore--;
+            document.querySelector(".starting-score").textContent = `💯 Score: ${startingScore}`;
+        } else {
+            displayedMsg.textContent = "You lose, Try again!"
+            document.querySelector(".starting-score").textContent = 0;
+        }
     }
 });
 
-
-
-
-
+//reload a whole page
+const restart = document.querySelector(".restart-btn");
+restart.addEventListener("click", () => {
+    location.reload();
+});
